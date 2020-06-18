a15d9251869c390d7f0d68d53d03b0025e2b31e6
sudhandar <sudhandar@gmail.com>
2020-06-18 21:41:48 +0530

    trial_stream

--- stream_script.py
+++ stream_script.py
@@ -2,3 +2,40 @@ from tweepy.streamer import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 import credentials
+
+class twitter_streamer():
+	" Class for streaming and processing live tweets"
+
+	def stream_tweets(self, fetched_data_file, hashtag_list):
+		listener = stdout_listener()
+		auth = OAuthHandler(credentials.CONSUMER_KEY, credentials.CONSUMER_KEY_SECRET)
+		auth.set_access_token(credentials.ACCESS_TOKEN, credentials.ACCESS_TOKEN_SECRET)
+		stream = Stream(auth, listener)
+		stream.filter(hashtag_list)
+
+
+class stdout_listener(StreamListener):
+
+	def __init__(self, fetched_data_file):
+		self.fetched_data_file = fetched_data_file
+
+	def on_data(self,data):
+		try:
+			print(data)
+			with open(self.fetched_data_file, 'a') as tf:
+				tf.write(data)
+			return True
+		except BaseException as e:
+			print("Error on data: %s" % str(e))
+			return True
+
+	def on_error(self, status):
+		print(status)
+
+if __name__ ==' __main__':
+
+	hashtag_list = ["MAGA2020"]
+	fetched_data_file = "test.json"
+
+	twitter = twitter_streamer()
+	twitter.stream_tweets(fetched_data_file, hashtag_list)
