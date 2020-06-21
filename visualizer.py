dbbbfc6355de20ae61fc43493eb87fee154e8105
sudhandar <sudhandar@gmail.com>
2020-06-21 19:33:18 +0530

    live updates

--- visualizer.py
+++ visualizer.py
@@ -1,12 +1,55 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+import pandas_datareader.data as web
+import datetime
+
+start = datetime.datetime(2015,1,1)
+end = datetime.datetime.now()
+
+stock ='TSLA'
+df = web.DataReader(stock, 'yahoo',start,end)
 
 app = dash.Dash()
-app.layout = html.Div(children = [html.H1('Sample Graph'),dcc.Graph(id = 'example', figure = {'data':[{'x':[1,2,3,4,5],'y':[5,6,7,2,1],'type':'line','name':'boats'},
-                                  {'x':[1,2,3,4,5],'y':[8,6,3,2,1],'type':'bar','name':'cats'},
-                                  ], 'layout' : { 'title': 'Basic Dash Sample'}}
-                                  )])
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+
+
+    html.Div(children='''
+    	Enter Stock Symbol of a Company:
+    '''),
+
+    dcc.Input(id = 'input', value = '', type='text'),
+    html.Div(id='output-graph')
+    
+])
+
+@app.callback(
+	Output(component_id = 'output-graph', component_property = 'children'),
+	[Input(component_id='input', component_property = 'value')]
+	)
+
+
+def update_graph(input_data):
+
+	start = datetime.datetime(2015,1,1)
+	end = datetime.datetime.now()
+	df = web.DataReader(input_data, 'yahoo', start, end)
+	return	dcc.Graph(
+		        id='example-graph',
+		        figure={
+		            'data': [
+		                {'x': df.index, 'y': df.Close, 'type': 'line', 'name': input_data}
+		            ],
+		            'layout': {
+		                'title': input_data
+		            }
+		        }
+		    )
+
 
 if __name__ == '__main__':
-	app.run_server(debug = True)
+    app.run_server(debug=True)
