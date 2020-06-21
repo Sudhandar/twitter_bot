d1b7b434ad44cbccbae9846a9d4a2d9672da0cf5
sudhandar <sudhandar@gmail.com>
2020-06-21 14:52:42 +0530

    sample graph

--- visualizer.py
+++ visualizer.py
@@ -3,7 +3,10 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 app = dash.Dash()
-app.layout = html.Div('Sample Graph')
+app.layout = html.Div(children = [html.H1('Sample Graph'),dcc.Graph(id = 'example', figure = {'data':[{'x':[1,2,3,4,5],'y':[5,6,7,2,1],'type':'line','name':'boats'},
+                                  {'x':[1,2,3,4,5],'y':[8,6,3,2,1],'type':'bar','name':'cats'},
+                                  ], 'layout' : { 'title': 'Basic Dash Sample'}}
+                                  )])
 
 if __name__ == '__main__':
 	app.run_server(debug = True)
