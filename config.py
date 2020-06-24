89677d86f1ba2dbd0140b21f41e97d4508ad059b
sudhandar <sudhandar@gmail.com>
2020-06-24 11:03:29 +0530

    environ

--- config.py
+++
@@ -1,9 +0,0 @@
-import tweepy
-
-auth = tweepy.OAuthHandler("eEGwOmCSyGHHq3WvPkr3yQ4jR", "yoaD1IHSpsi64bzl4jrA9UCsUAULbYbUdbcmPC5jgh6cHdQa1i")
-auth.set_access_token("1272135393878503424-Gf9rbA6gfpyRIdvZ2FMpbOViEjck2m", "FGm6D3NpNrbP5hjTF0Cabpussd4gzn1nfoQxkOPLe5pHH")
-
-# Create API object
-api = tweepy.API(auth, wait_on_rate_limit=True,
-    wait_on_rate_limit_notify=True)
-api.update_status("Test tweet from Tweepy Python")
--- sentiment_script.py
+++ sentiment_script.py
@@ -1,3 +1,9 @@
 from textblob import TextBlob
-from vaderSentiment.vaderSentiment import vaderSentimentIntensityAnalyzer
+
+text = '''The main issue is that relugolix was not tested in combination with NHTs or docetaxel, which are standard of care now for patients with metastatic prostate cancer. These data only apply to those patients who are receiving ADR monotherapy. #ASCO20'''
+
+analyzer = TextBlob(text)
+
+print(analyzer.sentiment)
+
 
