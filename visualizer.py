cf6468bb7ee475c3980378136c96471399c12836
Sudhandar <sudhandar@gmail.com>
2020-07-02 10:03:49 +0000

    gcp port issue fixed

--- visualizer.py
+++ visualizer.py
@@ -310,4 +310,4 @@ def update_pie_chart(n,sentiment_term):
             f.write(str(e))
             f.write('\n')
 if __name__ == '__main__':
-    app.run_server(debug = True)
+    app.run_server(host = '0.0.0.0', port = 8050)
