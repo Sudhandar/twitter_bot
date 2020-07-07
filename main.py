5d8ec815f2e5f4cae344af34bff0b608b936232f
sudhandar <sudhandar@gmail.com>
2020-07-07 10:58:45 +0530

    test

--- main.py
+++ main.py
@@ -491,5 +491,5 @@ def update_pie_chart(n,sentiment_term):
             f.write(str(e))
             f.write('\n')
 if __name__ == '__main__':
-    # app.run_server(host = '0.0.0.0',port= 8050,debug =True)
-    app.run_server(debug=True)
+    app.run_server(host = '0.0.0.0',port= 8050,debug =True)
+    # app.run_server(debug=True)
