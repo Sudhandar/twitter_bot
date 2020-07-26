73f3a4f0b7cf9346e2a0b02b5a29d0c9e8b5b04c
sudhandar <sudhandar@gmail.com>
2020-07-26 23:34:14 +0530

    New pie chart layout

--- live_updates.py
+++ live_updates.py
@@ -10,10 +10,19 @@ import pandas as pd
 import credentials
 from tweepy import OAuthHandler
 import dash_bootstrap_components as dbc
-
-
+import plotly.express as px
 
 POS_NEG_NEUT = 0.1
+def pos_neg_neutral(col):
+    if col >= POS_NEG_NEUT:
+        # positive
+        return 'Positive'
+    elif col <= -POS_NEG_NEUT:
+        # negative:
+        return 'Negative'
+    else:
+        return 'Neutral'
+
 
 app = dash.Dash(external_stylesheets=[dbc.themes.CYBORG])
 
@@ -26,6 +35,14 @@ app.layout = html.Div(
             interval= 1*1000,
             n_intervals = 0
         ),
+
+        dcc.Graph(id = 'pie-chart', animate = False),
+        dcc.Interval(
+            id='pie-update',
+            interval= 10*1000,
+            n_intervals = 0
+        ),
+        
 
 
 		html.Div(children = 'Recent Trends', style = {'textAlign' :'center'}),
@@ -57,124 +74,156 @@ def quick_color(s):
     else:
         return '#7f7f7f'
 
-def generate_table(df, max_rows=10):
-    return html.Table(className="responsive-table",
-                      children=[
-                          html.Thead(
-                              html.Tr(
-                                  children=[
-                                      html.Th(col.title()) for col in df.columns.values],
+# def generate_table(df, max_rows=10):
+#     return html.Table(className="responsive-table",
+#                       children=[
+#                           html.Thead(
+#                               html.Tr(
+#                                   children=[
+#                                       html.Th(col.title()) for col in df.columns.values],
                               
-                                  )
-                              ),
-                          html.Tbody(
-                              [
+#                                   )
+#                               ),
+#                           html.Tbody(
+#                               [
                                   
-                              html.Tr(
-                                  children=[
-                                      html.Td(data) for data in d
-                                      ], style={'color':'#FFFFFF',
-                                                'background-color':quick_color(d[2])}
-                                  )
-                               for d in df.values.tolist()])
-                          ]
-    )
+#                               html.Tr(
+#                                   children=[
+#                                       html.Td(data) for data in d
+#                                       ], style={'color':'#FFFFFF',
+#                                                 'background-color':quick_color(d[2])}
+#                                   )
+#                                for d in df.values.tolist()])
+#                           ]
+#     )
 
 
-@app.callback(Output('live-graph', 'figure'),
-		[Input('graph-update', 'n_intervals'),
+# @app.callback(Output('live-graph', 'figure'),
+# 		[Input('graph-update', 'n_intervals'),
+# 		Input(component_id = 'sentiment_term', component_property = 'value')])
+
+
+# def update_graph_scatter(n, sentiment_term):
+
+# 	try:
+
+# 		database_username = 'root'
+# 		database_password = ''
+# 		database_ip = 'localhost'
+# 		database_name = 'twitter_streaming'
+# 		database_connection = sqlalchemy.create_engine(
+# 		   'mysql+pymysql://{0}:{1}@{2}/{3}'.format(
+# 		       database_username, database_password,
+# 		       database_ip, database_name
+# 		   )
+# 		)
+
+# 		df = pd.DataFrame(database_connection.execute(" SELECT * FROM sentiment WHERE tweet LIKE %s ORDER BY unix DESC LIMIT 1000", ("%" + sentiment_term + "%",)),columns = ['unix','tweet','sentiment'])
+# 		df.sort_values('unix', inplace=True)
+# 		df['date'] = pd.to_datetime(df['unix'], unit = 'ms')
+# 		df.set_index('date', inplace = True)
+# 		df['sentiment_smoothed'] = df['sentiment'].rolling(int(len(df)/5)).mean()
+# 		df.dropna(inplace=True)
+# 		df = df.resample('1s').mean()
+
+# 		X = df.index[-100:]
+# 		Y = df.sentiment.values[-100:]
+
+# 		data = go.Scatter(
+# 			    x=list(X),
+# 			    y=list(Y),
+# 			    name='Scatter',
+# 			    mode= 'lines',
+# 			    )
+
+# 		return {'data': [data],'layout' : go.Layout(xaxis=dict(range=[min(X),max(X)]),
+# 		                                            yaxis=dict(range=[min(Y),max(Y)]),
+# 		                                            title = 'Term : {}'.format(sentiment_term))}
+
+# 	except Exception as e:
+# 		with open('errors.txt','a') as f:
+# 			f.write(str(e))
+# 			f.write('\n')
+
+# @app.callback(Output('recent_tweets_table', 'children'),
+# 		[Input('recent_tweets_table_update', 'n_intervals'),
+# 		Input(component_id = 'sentiment_term', component_property = 'value')])
+
+# def update_table(n, sentiment_term):
+
+# 	try:
+
+# 		database_username = 'root'
+# 		database_password = ''
+# 		database_ip = 'localhost'
+# 		database_name = 'twitter_streaming'
+# 		database_connection = sqlalchemy.create_engine(
+# 		   'mysql+pymysql://{0}:{1}@{2}/{3}'.format(
+# 		       database_username, database_password,
+# 		       database_ip, database_name
+# 		   )
+# 		)
+
+# 		df = pd.DataFrame(database_connection.execute(" SELECT * FROM sentiment WHERE tweet LIKE %s ORDER BY unix DESC LIMIT 10", ("%" + sentiment_term + "%",)),columns = ['unix','tweet','sentiment'])
+# 		df.sort_values('unix', inplace=True)
+# 		df['date'] = pd.to_datetime(df['unix'], unit = 'ms')
+# 		df.pop('unix')
+# 		df = df[['date','tweet','sentiment']]
+
+# 		return generate_table(df,10)
+
+# 	except Exception as e:
+# 		with open('errors.txt','a') as f:
+# 			f.write(str(e))
+# 			f.write('\n')
+
+# @app.callback(Output('recent_trends', 'children'),
+# 		[Input('trend_interval', 'n_intervals'),])
+
+
+# def recent_trends_df(n):
+
+# 	auth = OAuthHandler(credentials.CONSUMER_KEY, credentials.CONSUMER_KEY_SECRET)
+# 	auth.set_access_token(credentials.ACCESS_TOKEN, credentials.ACCESS_TOKEN_SECRET)
+# 	api = API(auth)
+# 	trends1 = api.trends_place(23424848)
+# 	data = trends1[0]['trends']
+# 	trends = [trend['name'] for trend in data][:10]
+
+# 	return trends
+
+@app.callback(Output('pie-chart', 'figure'),
+		[Input('pie-update', 'n_intervals'),
 		Input(component_id = 'sentiment_term', component_property = 'value')])
 
 
-def update_graph_scatter(n, sentiment_term):
+def update_graph_pie(n, sentiment_term):
 
-	try:
-
-		database_username = 'root'
-		database_password = ''
-		database_ip = 'localhost'
-		database_name = 'twitter_streaming'
-		database_connection = sqlalchemy.create_engine(
-		   'mysql+pymysql://{0}:{1}@{2}/{3}'.format(
-		       database_username, database_password,
-		       database_ip, database_name
-		   )
-		)
-
-		df = pd.DataFrame(database_connection.execute(" SELECT * FROM sentiment WHERE tweet LIKE %s ORDER BY unix DESC LIMIT 1000", ("%" + sentiment_term + "%",)),columns = ['unix','tweet','sentiment'])
-		df.sort_values('unix', inplace=True)
-		df['date'] = pd.to_datetime(df['unix'], unit = 'ms')
-		df.set_index('date', inplace = True)
-		df['sentiment_smoothed'] = df['sentiment'].rolling(int(len(df)/5)).mean()
-		df.dropna(inplace=True)
-		df = df.resample('1s').mean()
-
-		X = df.index[-100:]
-		Y = df.sentiment.values[-100:]
-
-		data = go.Scatter(
-			    x=list(X),
-			    y=list(Y),
-			    name='Scatter',
-			    mode= 'lines',
-			    )
-
-		return {'data': [data],'layout' : go.Layout(xaxis=dict(range=[min(X),max(X)]),
-		                                            yaxis=dict(range=[min(Y),max(Y)]),
-		                                            title = 'Term : {}'.format(sentiment_term))}
-
-	except Exception as e:
-		with open('errors.txt','a') as f:
-			f.write(str(e))
-			f.write('\n')
-
-@app.callback(Output('recent_tweets_table', 'children'),
-		[Input('recent_tweets_table_update', 'n_intervals'),
-		Input(component_id = 'sentiment_term', component_property = 'value')])
-
-def update_table(n, sentiment_term):
-
-	try:
-
-		database_username = 'root'
-		database_password = ''
-		database_ip = 'localhost'
-		database_name = 'twitter_streaming'
-		database_connection = sqlalchemy.create_engine(
-		   'mysql+pymysql://{0}:{1}@{2}/{3}'.format(
-		       database_username, database_password,
-		       database_ip, database_name
-		   )
-		)
-
-		df = pd.DataFrame(database_connection.execute(" SELECT * FROM sentiment WHERE tweet LIKE %s ORDER BY unix DESC LIMIT 10", ("%" + sentiment_term + "%",)),columns = ['unix','tweet','sentiment'])
-		df.sort_values('unix', inplace=True)
-		df['date'] = pd.to_datetime(df['unix'], unit = 'ms')
-		df.pop('unix')
-		df = df[['date','tweet','sentiment']]
-
-		return generate_table(df,10)
-
-	except Exception as e:
-		with open('errors.txt','a') as f:
-			f.write(str(e))
-			f.write('\n')
-
-@app.callback(Output('recent_trends', 'children'),
-		[Input('trend_interval', 'n_intervals'),])
-
-
-def recent_trends_df(n):
-
-	auth = OAuthHandler(credentials.CONSUMER_KEY, credentials.CONSUMER_KEY_SECRET)
-	auth.set_access_token(credentials.ACCESS_TOKEN, credentials.ACCESS_TOKEN_SECRET)
-	api = API(auth)
-	trends1 = api.trends_place(23424848)
-	data = trends1[0]['trends']
-	trends = [trend['name'] for trend in data][:10]
-
-	return trends
-
+    try:
+        database_username = 'root'
+        database_password = ''
+        database_ip = 'localhost'
+        database_name = 'twitter_streaming'
+        database_connection = sqlalchemy.create_engine(
+           'mysql+pymysql://{0}:{1}@{2}/{3}'.format(
+               database_username, database_password,
+               database_ip, database_name
+           )
+        )
+        df = pd.DataFrame(database_connection.execute(" SELECT * FROM sentiment_tweets WHERE tweet LIKE %s ORDER BY date DESC LIMIT 1000", ("%" + sentiment_term + "%",)),columns = ['unix','tweet','sentiment'])
+        df.sort_values('unix', inplace=True)
+        df['sentiment_shares'] = list(map(pos_neg_neutral, df['sentiment']))
+        df = df[df['sentiment_shares']!='Neutral']
+        sentiment_df = pd.DataFrame(df['sentiment_shares'].value_counts())
+        sentiment_df.reset_index(level=0 , inplace = True)
+        sentiment_df.columns = ['term','count']        
+        colors = ['green', 'red']
+        fig = px.pie(sentiment_df, values='count', names='term', color = 'term',title = 'Positive vs Negative sentiment for "{}" (longer-term)'.format(sentiment_term),color_discrete_map = {'Positive': 'green', 'Negative': 'red'})
+        return fig
+    except Exception as e:
+    	with open('errors.txt','a') as f:
+    		f.write(str(e))
+    		f.write('\n')
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False)
 
